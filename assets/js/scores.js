14132aa0bae3b41033ee56509c05cf64df47d0c7
Ygor <ygor.carmo@gmail.com>
2021-09-14 18:37:11 +1000

    fixed score layout

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,6 +1,7 @@
 // import score from './script.js';
 var clear = document.getElementById("done");
 var todoList = document.querySelector("#todos-list");
+var scoreDisplay = document.getElementById('score-ammount');
 
 var pontosLocal = JSON.parse(localStorage.getItem('pontos'));
 
@@ -11,10 +12,17 @@ clear.addEventListener('click', function(){
 // The following function renders items in a todo list as <li> elements
 function renderTodos() {
     for (var i = 0; i < pontosLocal.length; i++){
-        var pontosScored = pontosLocal[i]["initials"] + "    " + pontosLocal[i]["score"];
+        var pontosScored = pontosLocal[i]["initials"];
+        var numberScored = pontosLocal[i]["score"];
+
         var li = document.createElement("li");
+        var liNumber = document.createElement("li");
+
+        liNumber.textContent = numberScored;
         li.textContent = pontosScored;
+
         todoList.appendChild(li);
+        scoreDisplay.appendChild(liNumber);
     }
 }
 function init(){
